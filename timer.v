ce586e2de759d093d59e3d6fbe4ba4f723aaebb9
Hongli SHEN <149307453+renmiamu@users.noreply.github.com>
2024-11-22 14:24:28 +0800

    timer finished,not checked

--- timer.v
+++ timer.v
@@ -4,9 +4,12 @@ module timer (
     input power_state,
     input set_mode,
     input increase_key,
-    output reg [7:0] hour_tub_control,
-    output reg [7:0] minute_tub_control,
-    output reg [7:0] second_tub_control
+    output reg [7:0] hour_tub_control_1,
+    output reg [7:0] hour_tub_control_2,
+    output reg [7:0] minute_tub_control_1,
+    output reg [7:0] minute_tub_control_2,
+    output reg [7:0] second_tub_control_1,
+    output reg [7:0] second_tub_control_2,
 );
 
 reg [4:0] hours;
@@ -17,6 +20,20 @@ reg [31:0] counter;
 
 parameter ONE_SECOND = 32'b100000000;
 
+
+reg [7:0] segment_lut [0:9];
+initial begin
+    segment_lut[0] = 8'b11111100;
+    segment_lut[1] = 8'b01100000;
+    segment_lut[2] = 8'b11011010;
+    segment_lut[3] = 8'b11110010;
+    segment_lut[4] = 8'b01100110;
+    segment_lut[5] = 8'b10110110;
+    segment_lut[6] = 8'b10111110;
+    segment_lut[7] = 8'b11100000;
+    segment_lut[8] = 8'b11111110;
+    segment_lut[9] = 8'b11100110;
+end
 
 initial begin
     hours = 0;
@@ -74,7 +91,25 @@ always @(posedge clk,negedge reset) begin
     if (!reset) begin
         set_select <= 0;
     end else if (set_mode) begin
-        set_select <= (set_select + 1)%2；
+        set_select <= (set_select + 1)%2;
+    end
+end
+
+always @(posedge clk,negedge reset) begin
+    if (!reset) begin
+        hour_tub_control_1 <= 8'b00000000;
+        minute_tub_control_1 <= 8'b00000000;
+        second_tub_control_1 <= 8'b00000000;
+        hour_tub_control_2 <= 8'b00000000;
+        minute_tub_control_2 <= 8'b00000000;
+        second_tub_control_2 <= 8'b00000000;
+    end else begin
+        hour_tub_control_1 <= segment_lut[hours/10];
+        hour_tub_control_2 <= segment_lut[hours%10];
+        minute_tub_control_1 <= segment_lut[minutes/10];
+        minute_tub_control_2 <= segment_lut[minutes%10];
+        second_tub_control_1 <= segment_lut[seconds/10];
+        second_tub_control_2 <= segment_lut[seconds%10];
     end
 end
 
