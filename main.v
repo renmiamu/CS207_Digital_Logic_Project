2332486d03e2d07a43ab33ccc4345b5698be50de
XimingZheng <12311011@mail.sustech.edu.cn>
2024-12-09 21:35:14 +0800

    change main

--- main.v
+++ main.v
@@ -16,10 +16,12 @@ module main(
     output [7:0] tub_control_1,
     output [7:0] tub_control_2,
     output [7:0] tub_select,
-    output speaker
+    output speaker,
+    output pwm
 );
     wire cleaning_reminder;
-
+    wire [2:0] suction;
+    
     // 实例化 mode_change 模块
     mode_change modechanger(
         .clk(clk),
@@ -31,18 +33,24 @@ module main(
         .display_mode(display_mode),
         .set_select(set_select),
         .increase_key(increase_key),
-        .mode(mode),
+        .mode(suction),
         .countdown(countdown),
         .cleaning_reminder(cleaning_reminder),
         .tub_segments_1(tub_control_1),  // 连接小时段码
         .tub_segments_2(tub_control_2),  // 连接小时段码（可以根据需要修改连接或添加其他信号）
         .tub_select(tub_select)      // 连接分钟段码（同样可以更改为实际需要的信号）
     );
-    sound_reminder(
-    cleaning_reminder,
-    clk,
-    speaker
+
+    // 实例化 sound_reminder 模块
+    sound_reminder u_sound_reminder (
+        .cleaning_reminder(cleaning_reminder),  // 从 mode_change 模块传来的 cleaning_reminder 信号
+        .clk(clk),                             // 时钟信号
+        .suction(suction),
+        .speaker(speaker)                      // 输出到蜂鸣器的 PWM 信号
     );
-    assign reminder = cleaning_reminder;
+    
+    assign reminder = cleaning_reminder;  // 将 cleaning_reminder 信号赋给 reminder 输出
+    assign mode = suction;
+    assign pwm = 1'b0;
 
 endmodule
